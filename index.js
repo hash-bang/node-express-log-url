d4bb305663dd90df9e8bc08323668bca89a7c055
Matt Carter <m@ttcarter.com>
2019-06-25 14:28:54 +1000

    BUGFIX: Also look for username in req.user.email

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ module.exports = function(req, res, finish) {
 			path: unescape(req.originalUrl),
 			responseTime: Date.now() - req.requestTime.getTime(),
 			info: res.errorBody ? res.errorBody.toString() : '',
-			username: req.user && req.user.username ? req.user.username : undefined,
+			username:
+				req.user && req.user.username ? req.user.username
+				: req.user && req.user.email ? req.user.email
+				: undefined,
 		});
 
 		res.end.apply(res, arguments);
